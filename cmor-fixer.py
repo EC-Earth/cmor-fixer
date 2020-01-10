004d1d2fe5f8b0b5222b3435f2464241fcc8a7f4
gvdoord <g.vandenOord@gmail.com>
2020-01-10 14:32:42 +0100

    Escaping metadata fields starting with '#'

--- cmor-fixer.py
+++ cmor-fixer.py
@@ -61,7 +61,7 @@ def fix_file(path, write=True, keepid=False, forceid=False, metadata=None):
     if metadata is not None:
         for key, val in metadata.items():
             attname, attval = str(key), str(val)
-            if getattr(ds, attname, None) != attval:
+            if not attname.startswith('#') and getattr(ds, attname, None) != attval:
                 log.info("Setting metadata field %s to %s in %s" % (attname, attval, ds.filepath()))
                 if write:
                     setattr(ds, attname, attval)
