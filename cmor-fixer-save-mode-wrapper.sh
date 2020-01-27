bf0c6087ba15ae779eb886384a45e88e408f5959
treerink <thomas.reerink.knmi@gmail.com>
2020-01-27 11:23:13 +0100

    Add an argument: the number of cores. Include additional argument checks. Improve description. Redirect cmor-fixer output to log files. #16.

--- cmor-fixer-save-mode-wrapper.sh
+++ cmor-fixer-save-mode-wrapper.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 # Thomas Reerink
 #
-# This script is a wrapper of cmor-fixer. It only applies cmor-fixer changes when in the dataset at least one error is found. 
-# That means if all files in the entire dataset are correct, that never a tracking_id will be changed.
-# See #731 and the wiki https://dev.ec-earth.org/projects/cmip6/wiki/Correct-cmorised-data-with-the-cmor-fixer for further background.
+# This script is a wrapper of cmor-fixer. It only applies cmor-fixer changes when in the dataset at least one error is found.
+# That means, if all files in the entire dataset are correct, then no tracking_id will be changed anywhere.
+# See #731 and the wiki:
+#  https://dev.ec-earth.org/issues/731
+#  https://dev.ec-earth.org/projects/cmip6/wiki/Correct-cmorised-data-with-the-cmor-fixer for further background.
 #
-# This scripts requires one argument: The path of the directory with the cmorised data
+# This scripts needs two arguments:
+#
+# ${1} the first   argument is the number of cores (one node can be used).
+# ${2} the second  argument is path of the directory with the cmorised data.
 #
 # Run example:
-#  ./cmor-fixer-save-mode-wrapper.sh
+#  ./cmor-fixer-save-mode-wrapper.sh 1 CMIP6
 #
 
-if [ "$#" -eq 1 ]; then
+if [ "$#" -eq 2 ]; then
 
-   dir_with_cmorised_data=$1
-   number_of_cores=1
-  #number_of_cores=$2
+   number_of_cores=$1
+   dir_with_cmorised_data=$2
 
    olist_filename='list-of-modified-files.txt'
    olist_2_filename='list-of-modified-files-2.txt'
@@ -35,6 +39,23 @@ if [ "$#" -eq 1 ]; then
     exit 1
    fi
 
+   case "${number_of_cores}" in
+       ("" | *[!0-9]*)
+           echo -e "\e[1;31m Error:\e[0m"' Invalid value for the number of cores: ' ${number_of_cores} '. It should be [0-9][0-9]' >&2
+           exit 1
+   esac
+
+   if [ "${number_of_cores}" -lt 1 ] || [ "$1" -gt 100 ]; then
+       echo -e "\e[1;31m Error:\e[0m"' The value of number of cores ' ${number_of_cores} ' is out of range. Allowed range: 1-100.' >&2
+       exit 1
+   fi
+
+   if [ ! -d ${dir_with_cmorised_data} ]; then
+    echo
+    echo -e "\e[1;31m Error:\e[0m"' the directory ' ${dir_with_cmorised_data} ' does not exist.'
+    echo
+    exit 1
+   fi
 
 #  # First move list-of-modified-files.txt if they exists already:
 #  olist_filename='list-of-modified-files'
@@ -48,7 +69,7 @@ if [ "$#" -eq 1 ]; then
 
 
    # First run cmor-fixer in the save dry-run mode in order to figure out if there is any file with an error at all:
-   ./cmor-fixer.py --dry --verbose --olist --npp ${number_of_cores} ${dir_with_cmorised_data}
+   ./cmor-fixer.py --dry --verbose --olist --npp ${number_of_cores} ${dir_with_cmorised_data} &> cmor-fixer-messages-1.log
 
    # For testing the script with non-empty case:
   #echo ' Make non-empty for test only.' >> ${olist_filename}
@@ -57,7 +78,7 @@ if [ "$#" -eq 1 ]; then
   #sleep 1
    if [[ ! -e ${olist_filename} ]] ; then
     echo
-    echo ' Error: the file ' ${olist_filename} ' should have been produced.'
+    echo -e "\e[1;31m Error:\e[0m"' the file ' ${olist_filename} ' should have been produced.'
     echo
     exit 1
    fi
@@ -70,17 +91,17 @@ if [ "$#" -eq 1 ]; then
    fi
 
    # Create, before really applying the changes, the olist for the --forceid case:
-   ./cmor-fixer.py --dry --verbose --forceid --olist --npp ${number_of_cores} ${dir_with_cmorised_data}
+   ./cmor-fixer.py --dry --verbose --forceid --olist --npp ${number_of_cores} ${dir_with_cmorised_data} &> cmor-fixer-messages-2.log
 
    if [[ ! -e ${olist_2_filename} ]] ; then
     echo
-    echo ' Error: the file ' ${olist_2_filename} ' should have been produced.'
+    echo -e "\e[1;31m Error:\e[0m"' the file ' ${olist_2_filename} ' should have been produced.'
     echo
     exit 1
    fi
 
    # Apply the changes the olist for the --forceid case:
-   ./cmor-fixer.py --verbose --forceid --olist --npp ${number_of_cores} ${dir_with_cmorised_data}
+   ./cmor-fixer.py --verbose --forceid --olist --npp ${number_of_cores} ${dir_with_cmorised_data} &> cmor-fixer-messages-3.log
 
 
    diff ${olist_2_filename} ${olist_filename} >> ${diff_olists}
@@ -92,17 +113,17 @@ if [ "$#" -eq 1 ]; then
     rm -f ${diff_olists}
    else
     echo
-    echo ' Warning: the ' ${diff_olists} ' file is not empty. So it seems that the dry-run before and the modification run thereafter differ. Check for any interruption.'
+    echo -e "\e[1;33m Warning:\e[0m"' the ' ${diff_olists} ' file is not empty. So it seems that the dry-run before and the modification run thereafter differ.\e[1;33m Check for any interruption.\e[0m'
     echo
    fi
 
 
    # Final check: Check whether after modifying the errors, the dataser is now error free:
-   ./cmor-fixer.py --dry --verbose --olist --npp ${number_of_cores} ${dir_with_cmorised_data}
+   ./cmor-fixer.py --dry --verbose --olist --npp ${number_of_cores} ${dir_with_cmorised_data} &> cmor-fixer-messages-4.log
 
    if [[ ! -e ${olist_4_filename} ]] ; then
     echo
-    echo ' Error: the file ' ${olist_4_filename} ' should have been produced.'
+    echo -e "\e[1;31m Error:\e[0m"' the file ' ${olist_4_filename} ' should have been produced (in the post checking phase).'
     echo
     exit 1
    fi
@@ -113,20 +134,25 @@ if [ "$#" -eq 1 ]; then
     echo
    else
     echo
-    echo ' Warning: After correcting with cmor-fixer, it seems there are still errors in the dataset. Check the files by looking into ' ${olist_4_filename}
+    echo -e "\e[1;33m Warning:\e[0m"' After correcting with cmor-fixer, it seems there are still errors in the dataset. Check the files by looking into ' ${olist_4_filename}
     echo
    fi
 
-
-
    # Run the script ./versions.sh for instance to set all version directory names to January 20 2020:
-    echo ' The versions in this dataset are:'
+   echo
+   echo ' The versions.sh script detects the following versions in the final corrected dataset:'
    ./versions.sh -l ${dir_with_cmorised_data}
+  #echo ' In order to set one new version (recommended), for instance to February 20 2020, the versions.sh script can be run now by:'
+  #echo ' ./versions.sh -v v20200220 -m CMIP6/'
+   echo
 
 
 else
-    echo '  '
-    echo '  This scripts requires one argument, e.g.:'
-    echo '  ' $0 CMIP6/
-    echo '  '
+    echo
+    echo '  This scripts requires two arguments:'
+    echo '   The first  argument: is the number of cores (one node can be used).'
+    echo '   The second argument: is the path of the directory which contains the cmorised data.'
+    echo '  For instance:'
+    echo '  ' $0 1 CMIP6/
+    echo
 fi
